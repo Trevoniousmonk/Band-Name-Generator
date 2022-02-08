7c2ce7845457d0cba5a2db7cc9134a26314c53c1
TrevorBarton <56479176+Trevoniousmonk@users.noreply.github.com>
2022-02-08 13:13:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print(
+    "Is thinking of a band name stressing you out? Don't sweat it, just answer a few questions and we'll do all the dirty work for you:"
+)
+city = input("\nWhat city did you grow up in?\n")
+petname = input("What's your pet's name?\n")
+bandname = city + " " + petname
+print("Your band name is: " + bandname)
+print("\nCongratulations!")
